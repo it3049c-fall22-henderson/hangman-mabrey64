d0f7d802397725bc72b99f876cd76fd8a8a46855
mabrey64 <mabreydn@mail.uc.edu>
2022-10-08 12:32:09 -0400

    Updated JavaScript code
    Code for index.js and Hangman.js has been corrected and finished so the program runs smoothly.

--- resources/js/Hangman.js
+++ resources/js/Hangman.js
@@ -19,14 +19,12 @@ class Hangman {
    * The results is a json object that looks like this:
    *    { word: "book" }
    * */
-   getRandomWord(difficulty) {
-    debugger
-    return fetch(
-      `https://hangman-micro-service-bpblrjerwh.now.sh?difficulty=${difficulty}`
+    getRandomWord(difficulty) {
+     return fetch (
+      `https://hangman-micro-service.herokuapp.com/?difficulty=${difficulty}`
     )
       .then((r) => r.json())
       .then((r) => r.word);
-      debugger
   }
 
   /**
@@ -34,24 +32,25 @@ class Hangman {
    * @param {string} difficulty a difficulty string to be passed to the getRandomWord Function
    * @param {function} next callback function to be called after a word is reveived from the API.
    */
-  start(difficulty, next) {
+   async start(difficulty, next) {  
     // get word and set it to the class's this.word
     // clear canvas
     // draw base
     // reset this.guesses to empty array
     // reset this.isOver to false
     // reset this.didWin to false
-    const answer = this.getRandomWord(difficulty);
-    debugger
+    const answer = await this.getRandomWord(difficulty);
     this.word = answer;
-    next();
-    canvas.clearCanvas();
-    canvas.drawBase();
+    this.clearCanvas();
+    this.drawBase();
     this.guesses = [];
     this.isOver = false;
     this.didWin = false;
+    this.wrongGuesses = 0;
+    next();
   }
 
+  
   /**
    *
    * @param {string} letter the guessed letter.
@@ -66,38 +65,53 @@ class Hangman {
     // check if the word includes the guessed letter:
     //    if it's is call checkWin()
     //    if it's not call onWrongGuess()
-    const symbols = /^[a-zA-Z]+$/;
-    try {
-      if(letter == ``) throw `Please enter in a letter.`;
-      if(letter.match(symbols)) throw `Symbols or numbers are not in a word, or this word for that matter. Please enter a letter.`;
-      if(letter.length >> 0) throw `Only 1 letter entered at a time.`;
-    } catch {
-      return false;
+    const specialCharacters = /[0-9`!@#$%^&*()_+\-=\[\]{};':"\\|,.<>\/?~]/; 
+      if(letter === ``){
+          alert("Please enter in a letter.")
+          return false;
+      }
+      else if(letter.match(specialCharacters)){ 
+        alert ("Symbols or numbers are not in a word, or this word for that matter. Please enter a letter.")
+        return false;
+      }
+      else if(letter.length >> 1){
+        alert("Only 1 letter entered at a time.")
+        return false;
     }
-    letter.toLowerCase();
+      else {
+      letter.toLowerCase();
+    }
     if (this.guesses.includes(letter)){
-      throw `You've used this letter already. Enter a letter that hasn't been used.`;
+      alert ("You've used this letter already. Enter a letter that hasn't been used.")
+      return false;
     }
     else {
       this.guesses.push(letter);
     }
     if(this.word.includes(letter))
     {
-      checkWin();
+      this.checkWin();
     }
     else {
-      onWrongGuess();
+      this.onWrongGuess();
     }
   }
 
   checkWin() {
     // using the word and the guesses array, figure out how many remaining unknowns.
     // if zero, set both didWin, and isOver to true
-    const lettersRemaining = this.word.match(this.guesses);
-    if (lettersRemaining == `0`)
+    let lettersRemaining = this.word.length;
+    for (let i = 0; i < this.guesses.length; i++) {
+      for (let j = 0; j < this.word.length; j++) {
+        if (this.word.charAt(j) == this.guesses[i]) {
+          lettersRemaining--;
+        }
+      }
+    }
+    if (lettersRemaining === 0)
     {
-      this.didWin() = true;
-      this.isOver() = true;
+      this.didWin = true;
+      this.isOver = true;
     }
   }
 
@@ -107,48 +121,33 @@ class Hangman {
    * if the number wrong guesses is 6, then also set isOver to true and didWin to false.
    */
   onWrongGuess() {
-    if (this.guesses.length = 1)
-    {
-      drawHead();
-    }
-    else if (this.guesses.length = 2)
-    {
-      drawHead();
-      drawBody();
-    }
-    else if (this.guesses.length = 3)
-    {
-      drawHead();
-      drawBody();
-      drawLeftArm();
-    }
-    else if (this.guesses.length = 4)
-    {
-      drawHead();
-      drawBody();
-      drawLeftArm();
-      drawRightArm();
-    }
-    else if (this.guesses.length = 5)
-    {
-      drawHead();
-      drawBody();
-      drawLeftArm();
-      drawRightArm();
-      drawLeftLeg();
-    }
-    else (this.guesses.length = 6)
-    {
-      drawHead();
-      drawBody();
-      drawLeftArm();
-      drawRightArm();
-      drawLeftLeg();
-      drawRightLeg();
-      this.isOver() = true;
-      this.didWin() = false;
-    }
+    switch (this.wrongGuesses) {
+    case 0:
+      this.drawHead();
+      break;
+    case 1:
+      this.drawBody();
+      break;
+    case 2:
+      this.drawLeftArm();
+      break;
+    case 3:
+      this.drawRightArm();
+      break;
+    case 4:
+      this.drawLeftLeg();
+      break;
+    case 5:
+      this.drawRightLeg();
+      this.isOver = true;
+      this.didWin = false;
+      break;
+    default:
+    this.isOver = false;
+    break;
   }
+  this.wrongGuesses++;
+}
 
   /**
    * This function will return a string of the word placeholder
@@ -156,9 +155,20 @@ class Hangman {
    * i.e.: if the word is BOOK, and the letter O has been guessed, this would return _ O O _
    */
   getWordHolderText() {
-    const word = this.answer;
-    word.replace(/[a-z]/g, `_`).split(``).join(` `);
-    return word;
+    let placeHolder = "Word: ";
+    let wordUnderscores = this.word.split("");
+    for (let i = 0; i < wordUnderscores.length; i++){
+      wordUnderscores[i] = "_";
+    }
+    for (let i = 0; i < this.guesses.length; i++){
+      for (let j = 0; j < this.word.length; j++){
+        if (this.word.charAt(j) === this.guesses[i]) {
+          wordUnderscores[j] = this.guesses[i];
+        }
+      }
+    }
+    placeHolder += wordUnderscores.join(" ");
+    return placeHolder;
   }
 
   /**
@@ -168,8 +178,9 @@ class Hangman {
    * Hint: use the Array.prototype.join method.
    */
   getGuessesText() {
-    this.guesses.join(`, `);
-    return `Guesses: ${this.guesses}`;
+    let guessHolder = "Guesses: "
+    guessHolder += this.guesses.join(", ");
+    return guessHolder;
   }
 
   /**
@@ -191,42 +202,43 @@ class Hangman {
 
   drawHead() {
     this.ctx.beginPath();
-    this.ctx.arc(95, 50, 40, 0, 2 * Math.PI);
+    this.ctx.arc(250, 100, 40, 0, 2 * Math.PI);
     this.ctx.stroke();
   }
 
   drawBody() {
     this.ctx.beginPath();
-    this.ctx.moveTo(95, 75);
-    this.ctx.lineTo(95, 145);
+    this.ctx.moveTo(250, 140);
+    this.ctx.lineTo(250, 220);
     this.ctx.stroke();
   }
 
   drawLeftArm() {
     this.ctx.beginPath();
-    this.ctx.moveTo(95, 85);
-    this.ctx.lineTo(125, 110);
+    this.ctx.moveTo(250, 160);
+    this.ctx.lineTo(280, 190);
     this.ctx.stroke();
   }
 
   drawRightArm() {
     this.ctx.beginPath();
-    this.ctx.moveTo(95, 85);
-    this.ctx.lineTo(65, 110);
+    this.ctx.moveTo(250, 160);
+    this.ctx.lineTo(220, 190);
     this.ctx.stroke();
   }
 
   drawLeftLeg() {
     this.ctx.beginPath();
-    this.ctx.moveTo(95, 115);
-    this.ctx.lineTo(125, 170);
+    this.ctx.moveTo(250, 220);
+    this.ctx.lineTo(280, 250);
     this.ctx.stroke();
+
   }
 
   drawRightLeg() {
     this.ctx.beginPath();
-    this.ctx.moveTo(95, 115);
-    this.ctx.lineTo(65, 170);
+    this.ctx.moveTo(250, 220);
+    this.ctx.lineTo(220, 250);
     this.ctx.stroke();
   }
 }
--- resources/js/index.js
+++ resources/js/index.js
@@ -1,5 +1,24 @@
+
+/**
+ * Stuff to fix tomorrow for future self:
+ * 1. Show letters in guesses.
+ * 2. Same thing as 1 but fill in the underscores with the letters as they are guessed.
+ * 3. Move the person to match the base of the gallows.
+ * 4. Fix index.html so that the startWrapper and gameWrapper are visible and/or hidden when needed.
+ * 5. Fix index.js code so that it immediately DOESN'T have the user lose the game after guessing 1 letter.
+ * 6. Fix Hangman.js code so that only letters are allowed to be enter and not return a false immediately.
+ * 7. Figure out how to set a const to a method (the opposite of calling a function and setting it to the const).
+ * After steps 1-7 are done, make sure the whole thing still works properly according to instructions,
+ * Then I can add in canvas and css to make the program look good.
+ * From
+ * -Past Devon
+ * P.S. Don't immediately use someone's (I.E. the TA's) code unless you know what it does. Use it as an example of what you CAN do, no copy paste. Figure out a way to make your own code work first, or find a way through da internet.
+ * And don't give up. You are better than this and have people that know you can do better and see this through.
+ */
+
+
 // START + DIFFICULTY SELECTION
-const startWrapper = document.getElementById(`startWrapper`);
+const startWrapper = document.getElementById('startWrapper');
 const difficultySelectForm = document.getElementById(`difficultySelect`);
 const difficultySelect = document.getElementById(`difficulty`);
 
@@ -14,14 +33,16 @@ const guessInput = document.getElementById(`guessInput`);
 
 // GAME RESET BUTTON
 const resetGame = document.getElementById(`resetGame`);
+const guessSubmitButton = document.getElementById(`guessSubmitButton`);
 
 // CANVAS
 let canvas = document.getElementById(`hangmanCanvas`);
 
+
 // The following Try-Catch Block will catch the errors thrown
 try {
+  let game = new Hangman(canvas);
   // Instantiate a game Object using the Hangman class.
-let game = new Hangman(canvas);
   // add a submit Event Listener for the to the difficultySelectionForm
   //    get the difficulty input
   //    call the game start() method, the callback function should do the following
@@ -31,11 +52,13 @@ let game = new Hangman(canvas);
   //       4. call the game getGuessessText and set it to the guessesText
   difficultySelectForm.addEventListener(`submit`, function (event) {
     const selectedDifficulty = difficultySelect.options[difficultySelect.selectedIndex].value;
-    game.start(selectedDifficulty);
-    startWrapper.classList.remove(`d-none`);
-    gameWrapper.classList.add(`d-none`);
-    wordHolderText = game.getWordHolderText();
-    guessesText = game.getGuessesText();
+    game.start(selectedDifficulty, function next(wordAnswer){
+      wordAnswer = game.word;
+      startWrapper.classList.add('hidden');
+      gameWrapper.classList.remove('hidden');
+      wordHolderText.innerHTML = game.getWordHolderText();
+      guessesText.innerHTML = game.getGuessesText();
+    });
     event.preventDefault();
   });
 
@@ -55,31 +78,42 @@ let game = new Hangman(canvas);
   guessForm.addEventListener(`submit`, function (e) {
     const input = guessInput.value;
     game.guess(input);
-    game.getWordHolderText() = wordHolderText;
-    game.getGuessesText() = guessesText;
-    guessInput.value = "";
-    if (game.isOver() == true){
-      document.getElementById(`guessInput`).disabled = true;
-      document.getElementById(`guessSubmitButton`).disabled = true;
-      document.getElementById(`resetGame`).style.visibility = `visible`;
-    }
-    if (game.didWin() == true){
-      alert(`You have won the game!`)
-    }
-    else {
-      alert(`Better luck next time...`)
-    }
+    wordHolderText.innerHTML = game.getWordHolderText();
+    guessesText.innerHTML = game.getGuessesText();
+    guessInput.value = "";  
+      checkWinOrLose();
+    event.preventDefault();
   });
+
+function checkWinOrLose(){
+    if (game.isOver === true){
+      if (game.didWin === true){
+        guessInput.disabled = true;
+        guessSubmitButton.disabled = true;
+        resetGame.classList.remove('hidden');
+        alert(`You have won the game!`)
+      }
+      else {
+        guessInput.disabled = true;
+        guessSubmitButton.disabled = true;
+        resetGame.classList.remove('hidden');
+        alert(`Better luck next time...`)
+      };
+    }
+  }
 
   // add a click Event Listener to the resetGame button
   //    show the startWrapper
   //    hide the gameWrapper
   resetGame.addEventListener(`click`, function (e) {
-    startWrapper.classList.add(`d-none`);
-    gameWrapper.classList.remove(`d-none`);
+    startWrapper.classList.remove('hidden');
+    gameWrapper.classList.add('hidden');
+    guessInput.disabled = false;
+    guessSubmitButton.disabled = false;
+    resetGame.classList.add('hidden');
   });
 
 } catch (error) {
-  //console.error(error);
+  console.error(error);
   alert(error);
 }
